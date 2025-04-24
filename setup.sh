a46cf3cd216013dfc43796aacaa1772cac4ed6e1
mhiroe <hmregist@gmail.com>
2025-04-24 14:13:32 +0900

    gemini

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 # 必ず親ディレクトリから実行して展開する
 # > ./mizchi/setup.sh
 
-TEMPLATE_FILES=(.cline .github .hooks modules .clinerules .roomodes deno.json deno.lock)
+TEMPLATE_FILES=(.cline .github .hooks modules .clinerules .roomodes deno.json deno.lock .gemini)
 
 for file in ${TEMPLATE_FILES[@]}
 do
